dddbe914ee561011cb07243c6e88f309a30657e9
Cj-bc <cj-bc@y-modify.org>
2018-02-25 18:06:03 +0900

    Add: first formula. v0.0.1

---
+++ owncloud.rb
@@ -0,0 +1,21 @@
+ class Owncloud < Formula
+  version = "0.0.1"
+  desc "the simplest utilitu tool for owncloud"
+  homepage "https://github.com/Cj-bc/owncloud"
+  url "https://github.com/Cj-bc/owncloud/archive/v"+ version + ".tar.gz"
+  head "https://github.com/Cj-bc/owncloud.git"
+  sha256 "fd9e28eb345be363b099aea8e7f49b146477bd72af72adbb4cd8ac916adf593f"
+
+
+   def install
+     ohai "renaming owncloud.sh"
+     system "mv", "owncloud.sh", "owncloud"
+     bin.install "owncloud"
+     prefix.install Dir["func"], Dir["text"], ".sourcefiles", "test.sh"
+     prefix.install "docs/config"
+  end
+
+   test do
+     system prefix+"/test.sh"
+  end
+end
