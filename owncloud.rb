57be46689930950b7dc3e4d4ed668a4570dd610f
Cj-bc <cj-bc@y-modify.org>
2018-03-03 12:45:52 +0900

    ver: v0.1.2

--- owncloud.rb
+++ owncloud.rb
@@ -1,10 +1,10 @@
  class Owncloud < Formula
-  version = "0.1.1"
+  version = "0.1.2"
   desc "the simplest utility tool for owncloud"
   homepage "https://github.com/Cj-bc/owncloud"
   url "https://github.com/Cj-bc/owncloud/archive/v"+ version + ".tar.gz"
   head "https://github.com/Cj-bc/owncloud.git"
-  sha256 "f3f5c25fef7c3a5b4281ca0ae9720b39a2fe3b98bfa4d59810aa3c0b2143eedf"
+  sha256 "3019ca6c0b2799d79421e3319b0c47ab802894c69bd1ebdcfddf58ba618bad7a"
 
 
    def install
