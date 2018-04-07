769ae68e98f59e8ef5c1431ae2ad1ca1f50d00a0
Cj-bc <cj-bc@y-modify.org>
2018-04-07 23:29:15 +0900

    ver: 0.3.1

--- owncloud.rb
+++ owncloud.rb
@@ -1,20 +1,38 @@
  class Owncloud < Formula
-  version = "0.2.0"
+  version = "0.3.1"
   desc "the simplest utility tool for owncloud"
   homepage "https://github.com/Cj-bc/owncloud"
   url "https://github.com/Cj-bc/owncloud/archive/v"+ version + ".tar.gz"
   head "https://github.com/Cj-bc/owncloud.git"
-  sha256 "0a9c2e130bb59068092f98130da408751151080af66fcde35fe239df1f57cbf5"
+  sha256 "3ad0ce45d61d1992bef43f4921672de80a2e9005ed796b3894d9c7fc021fb36c"
 
+  depends_on "bash-completion"
 
    def install
      ohai "renaming owncloud.sh"
      system "mv", "owncloud.sh", "owncloud"
      bin.install "owncloud"
      prefix.install Dir["func"], Dir["text"], ".sourcefiles", "test.sh"
+     ohai "install bash completion..."
+     ohai "checking bash-completion path..."
+     bash_completion.install "completion-owncloud.sh"
   end
 
    test do
-     system "`brew --prefix`/owncloud/"+ version +"/test.sh"
+     #system "`brew --prefix`/owncloud/"+ version +"/test.sh"
+    #system "echo this is test file!!!! hello world", ">", "test.txt"
+    system "echo", "test: test.txt generated"
+    system "owncloud",  "mkdir", "testdir"
+    system "owncloud", "post", "test.txt", "testdir/test.txt"
+    system "owncloud",  "ls", "testdir"
+    system "owncloud",  "mv", "testdir/test.txt", "test.txt"
+    system "owncloud",  "get", "test.txt"
+    system "echo", "data:\n>  "
+    system "cat", "test.txt"
+    system "owncloud" 
+    system "echo", "\"test start: rm\""
+    system "owncloud", "rm", "testdir;owncloud rm test.txt"
+    system "rm", "test.txt", "&&", "echo", "test: test.txt removed"
+
   end
 end
